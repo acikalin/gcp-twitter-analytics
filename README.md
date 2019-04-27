aee0c536723d78b97ac8c9f6c9359af3d958c9fa
Acikalin, Emrah <emrahacikalin@gmail.com>
2019-04-27 11:40:26 +0300

    Intiial Commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# gcp-twitter-analytics
+Analyze tweets with a Serverless Data Processing pipeline on Google Cloud Platform.
+
+The architecture consists of:
+- a Google Container Engine cluster running a Python application that gathers tweets and sends them to Google Pub/Sub;
+- a Google Cloud Pub/Sub topic;
+- a Google Cloud Dataflow pipeline that reads from the Pub/Sub topic and uses the Natural Language API to retrieve the sentiment of each tweet;
+- a Google BigQuery dataset containing two tables, respectively for "raw" and "annotated" tweets.
+- a Google App Engine shows results
+
+## Setup
+The repository contains a bash script that automates most of the work. However, there is still something you have to do yourself:
+
+- Create a new Google Cloud Platform project (see https://support.google.com/cloud/answer/6251787?hl=en for instructions).
+- Enable the Natural Language API from the Cloud Console (https://console.cloud.google.com/apis/api/language.googleapis.com/overview).
+- Open Google Cloud Shell.
+- Within Cloud Shell, clone the Git Repository: `git clone https://github.com/acikalin/gcp-twitter-analytics.git gcp-twitter-analytics`.
+- Edit twitter-stream.yaml. Set the Twitter authentication information to the values you noted when setting up your Twitter application (CONSUMERKEY,CONSUMERSECRET, ACCESSTOKEN, and ACCESSTOKENSEC).
+- [Create a Twitter application](https://apps.twitter.com/app/new) and paste the required information in the gcp-twitter-analytics/k8s-twitter-to-pubsub/twitter-stream.yaml file. Use your preferred text editor, like `vi` or `nano`: `nano gcp-twitter-analytics/k8s-twitter-to-pubsub/twitter-stream.yaml`
+- launch the start.sh file to provision and start the processing pipeline: `bash gcp-twitter-analytics/start.sh`.
+
+## Credits
+The Python appplication that collects tweets and publish them on Pub/Sub comes from the really nice example "Real-Time Data Analysis with Kubernetes, Cloud Pub/Sub, and BigQuery" published here: https://cloud.google.com/solutions/real-time/kubernetes-pubsub-bigquery.
---
+++ springboot-appengine-standard/src/main/java/com/example/appengine/demos/springboot/HelloworldController.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.appengine.demos.springboot;
+
+import com.google.cloud.bigquery.BigQuery;
+import com.google.cloud.bigquery.BigQueryOptions;
+import com.google.cloud.bigquery.QueryJobConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloworldController {
+  @GetMapping("/")
+  public String hello() throws InterruptedException {
+
+    BigQuery bigquery = BigQueryOptions.getDefaultInstance().getService();
+
+    String query = "#standardSQL\n" +
+            "WITH words_in_tweets AS (\n" +
+            "  SELECT \n" +
+            "    ARRAY(\n" +
+            "      SELECT AS STRUCT word, COUNT(1) AS cnt \n" +
+            "      FROM UNNEST(REGEXP_EXTRACT_ALL(text, r'[\\w_]+')) AS word \n" +
+            "      GROUP BY word\n" +
+            "      HAVING NOT REGEXP_CONTAINS(word, r'^\\d+$')\n" +
+            "    ) AS words\n" +
+            "  FROM `tweets.tweets`\n" +
+            "),\n" +
+            "words_in_corpus AS (\n" +
+            "  SELECT word, SUM(cnt) AS cnt\n" +
+            "  FROM words_in_tweets, UNNEST(words) AS words\n" +
+            "  GROUP BY word\n" +
+            ")\n" +
+            "SELECT * \n" +
+            "FROM words_in_corpus\n" +
+            "ORDER BY cnt DESC\n" +
+            "Limit 10";
+    // Create a query request
+    QueryJobConfiguration queryConfig =
+            QueryJobConfiguration.newBuilder(query).build();
+    // Read rows
+    return "Hello world - springboot-appengine-standard!" + bigquery.query(queryConfig).toString();
+  }
+}
