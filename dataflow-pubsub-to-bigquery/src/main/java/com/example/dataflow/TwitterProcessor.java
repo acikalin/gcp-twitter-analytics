6ecd0af9aafa8f69034e52a00358cc90bc9bcd85
Acikalin, Emrah <emrahacikalin@gmail.com>
2019-04-29 02:25:42 +0300

    no message

--- dataflow-pubsub-to-bigquery/src/main/java/com/example/dataflow/TwitterProcessor.java
+++ dataflow-pubsub-to-bigquery/src/main/java/com/example/dataflow/TwitterProcessor.java
@@ -1,13 +1,11 @@
 package com.example.dataflow;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.google.api.services.bigquery.model.TableFieldSchema;
 import com.google.cloud.language.v1.*;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
-import net.minidev.json.parser.JSONParser;
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO;
 import org.apache.beam.sdk.transforms.*;
@@ -27,6 +25,9 @@ import com.google.cloud.language.v1.AnalyzeSyntaxRequest;
 import com.google.cloud.language.v1.Document.Type;
 import com.google.cloud.language.v1.EncodingType;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TwitterProcessor {
 
     private static final Logger LOG = LoggerFactory.getLogger(TwitterProcessor.class);
@@ -40,31 +41,26 @@ public class TwitterProcessor {
         options.setAutoscalingAlgorithm(AutoscalingAlgorithmType.THROUGHPUT_BASED);
         options.setMaxNumWorkers(3);
         String projectId = options.getProject();
-
         LOG.info("MERHABA");
-
         Pipeline pipeline = Pipeline.create(options);
         pipeline.apply("TweetsReadPubSub", PubsubIO.readStrings().fromTopic("projects/" + projectId + "/topics/twitter"))
                 .apply("ConvertDataToTableRows", ParDo.of(new DoFn<String, TableRow>() {
                     @ProcessElement
                     public void processElement(ProcessContext c) {
+                        LOG.info("FATMA");
                         TableRow row = new TableRow();
                         try {
                             LOG.info("TRLXYT: " + c.element());
-                            JSONParser parser = new JSONParser();
-                            Object obj = parser.parse(c.element());
-                            JSONObject jsonObject = (JSONObject) obj;
-                            String text = (String) jsonObject.get("text");
-                            String lang = (String) jsonObject.get("lang");
+                            JsonObject jsonTweet = new JsonParser().parse(c.element()).getAsJsonObject();
 
-                            if (obj != null && text!= null && lang != null) {
+                            if (jsonTweet != null && jsonTweet.getAsJsonPrimitive("text") != null && jsonTweet.getAsJsonPrimitive("lang") != null) {
 
-                                if (lang.equalsIgnoreCase("en") && text.contains("rize")) {
+                                if ((jsonTweet.getAsJsonPrimitive("text").getAsString().toLowerCase().contains("umraniye")) && jsonTweet.getAsJsonPrimitive("lang").getAsString().equalsIgnoreCase("en")) {
 
                                     LOG.info("Processing tweet: " + c.element());
 
-                                    Sentiment sentiment = analyzeSentiment(text);
-                                    List<Token> tokens = analyzeSyntaxText(text);
+                                    Sentiment sentiment = analyzeSentiment(jsonTweet.getAsJsonPrimitive("text").getAsString());
+                                    List<Token> tokens = analyzeSyntaxText(jsonTweet.getAsJsonPrimitive("text").getAsString());
                                     JSONArray jsonTokens = new JSONArray();
                                     for (Token token : tokens) {
                                         JSONObject jsonToken = new JSONObject();
@@ -76,12 +72,13 @@ public class TwitterProcessor {
                                             .set("syntax", jsonTokens.toJSONString())
                                             .set("score", sentiment.getScore())
                                             .set("magnitude", sentiment.getMagnitude());
+                                    c.output(row);
                                 }
                             }
                         } catch (Exception e) {
                             row.set("tweet_object", e.toString());
+                            c.output(row);
                         }
-                        c.output(row);
                     }
                 }))
                 .apply("InsertToBigQuery", BigQueryIO
