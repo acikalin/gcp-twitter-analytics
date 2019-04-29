c4cfa34e8b40836a2e1732bca44a70fe65836b1c
Acikalin, Emrah <emrahacikalin@gmail.com>
2019-04-29 13:25:28 +0300

    no message

--- k8s-twitter-to-pubsub/controller.py
+++ k8s-twitter-to-pubsub/controller.py
@@ -21,9 +21,7 @@ import os
 
 script = os.environ['PROCESSINGSCRIPT']
 
-if script == 'pubsub-to-bigquery':
-    os.system("python pubsub-to-bigquery.py")
-elif script == 'twitter-to-pubsub':
+if script == 'twitter-to-pubsub':
     os.system("python twitter-to-pubsub.py")
 else:
     print "unknown script %s" % script
