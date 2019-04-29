9426d0edc0c405a65202edf48ab76addef0da1c3
Acikalin, Emrah <emrahacikalin@gmail.com>
2019-04-29 12:10:38 +0300

    no message

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PROJECT_ID=acikalin3
+PROJECT_ID=acikalin2018
 gcloud config set project $PROJECT_ID
 gcloud config set compute/zone us-west1-a
 gcloud config set compute/region us-west1
